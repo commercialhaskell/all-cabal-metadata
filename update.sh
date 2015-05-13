eea92345dfa05f2547163b573ab1b7397aab17ae
Michael Snoyman <michael@snoyman.com>
2015-05-13 05:25:49 +0300

    Travis job

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -xue
+
+wget https://s3.amazonaws.com/stackage-travis/all-cabal-hashes-tool/all-cabal-metadata-tool.bz2
+bunzip2 all-cabal-metadata-tool.bz2
+chmod +x all-cabal-metadata-tool
+mv all-cabal-metadata-tool /tmp
+/tmp/all-cabal-metadata-tool
+
+if [ -n "$(git status --porcelain)" ]
+then
+    git add -A
+    git commit -m "Update from Hackage at $(date --utc --iso=sec)" --gpg-sign=D6CF60FD
+    git push git@github.com:commercialhaskell/all-cabal-metadataHEAD:master
+else
+    echo No changes present
+fi
